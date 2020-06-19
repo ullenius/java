d2a07841b83ecf41a00620308984a79d04520d89
Anosh D. Ullenius <anosh@anosh.se>
2020-06-19 17:29:56 +0200

    Implement comparable

--- src/main/java/dev/personnummer/PersonalNumber.java
+++ src/main/java/dev/personnummer/PersonalNumber.java
@@ -1,9 +1,14 @@
 package dev.personnummer;
 
-public class PersonalNumber extends Personnummer {
+public class PersonalNumber extends Personnummer implements Comparable<PersonalNumber> {
 	
 	public PersonalNumber(CharSequence number) {
 		super(number, a -> a);
 	}
 
+	@Override
+	public int compareTo(PersonalNumber o) {
+		return Long.compare(this.asLong(), o.asLong());
+	}
+	
 }
--- src/main/java/dev/personnummer/Samordningsnummer.java
+++ src/main/java/dev/personnummer/Samordningsnummer.java
@@ -1,8 +1,13 @@
 package dev.personnummer;
 
-public class Samordningsnummer extends Personnummer {
+public class Samordningsnummer extends Personnummer implements Comparable<Samordningsnummer> {
 
 	public Samordningsnummer(CharSequence number) {
 		super(number, a -> (a-60));
+	}
+	
+	@Override
+	public int compareTo(Samordningsnummer o) {
+		return Long.compare(this.asLong(), o.asLong());
 	}
 }
